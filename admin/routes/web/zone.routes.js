54deb911b4437051cdf3e11e228dda3fed09acf2
ice-009 <21bma009@nith.ac.in>
2023-10-26 18:12:38 +0530

    get zone by id

--- admin/routes/web/zone.routes.js
+++ admin/routes/web/zone.routes.js
@@ -4,32 +4,28 @@ const { AdminZoneController } = require('../../controller');
 const ZoneModel =  require('../../../model/zone')
 const router = express.Router();
 
-// View form for creating a new zone
 router.get('/create', adminToken, AdminZoneController.homeZone);
 
-// Create a new zone
 router.post('/create', adminToken, AdminZoneController.createZone);
 
 
 router.get(
     '/',async (req,res)=>  {try {
-      const zones = await ZoneModel.find(); // Retrieve all zones from the database
-      res.json(zones); // Send the zones as a JSON response
+      const zones = await ZoneModel.find(); 
+      res.json(zones); 
     } catch (error) {
       res.status(500).json({ error: 'Failed to retrieve zones' });
     }
   });
   router.get('/:id', async (req, res) => {
     try {
-      const zoneId = req.params.id; // Access the :id parameter
-      const zone = await ZoneModel.findOne({ zoneId }); // Query the Zone model
+      const zoneId = req.params.id; 
+      const zone = await ZoneModel.findOne({ zoneId }); 
   
       if (!zone) {
-        // Handle the case where the zone with the specified ID is not found
         return res.status(404).json({ error: 'Zone not found' });
       }
-  
-      // Respond with the zone information
+
       res.json(zone);
     } catch (error) {
       // Handle other errors
