5375477e1f8f58524a7479a69b8bdc49a1c3e49f
ice-009 <21bma009@nith.ac.in>
2023-12-11 11:33:52 +0530

    cors added

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ const dotenv = require('dotenv')
 const path = require('path')
 const hbs = require('hbs')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const process = require('process')
 
 const app = express()
 app.use(cookieParser());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet())
 app.use(express.json())
