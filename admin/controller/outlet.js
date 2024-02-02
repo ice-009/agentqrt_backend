181a28a40f1385d129b10f56bbbaf8725ee69b24
ice-009 <21bma009@nith.ac.in>
2024-02-02 14:15:17 +0530

    styling and dynmaic made;

--- admin/controller/outlet.js
+++ admin/controller/outlet.js
@@ -2,6 +2,7 @@ const {getOutletPage} =  require('../service/outlet');
 
 const getOutletPageContr = async function(req,res){
     const outlet = await getOutletPage(req.params.id);
+    console.log("out",outlet);
     res.render('admin/outlet/home',{outlet})
 }
 
